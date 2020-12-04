c7015cef734c5fc6bf1ed1d0038c978f13ea954b
BabyOscar11 <muhanli2011@gmail.com>
2020-12-04 16:45:35 -0500

    Changed some prompt to input and added button

--- typing.js
+++ typing.js
@@ -1,11 +1,16 @@
 window.onload = function () {
 	let paragraph = document.getElementById("message");
-	let typingMessage = prompt("What do you want to type?");
-	let color = prompt("What color do you want the message to be, if you want a shade of something do it like 'lightblue' not 'light blue'");
-	let j = 1000;
-	for (let i = 0; i < typingMessage.length; i++) {
-		paragraph.style.color = color.toLowerCase();
-		setTimeout(() => paragraph.textContent += typingMessage[i], j);
-		j += 50;
-	}
+	let typingMessage = document.getElementById("type-input");
+	let typingButton = document.getElementById("type-button");
+	typingButton.addEventListener("click", () => {
+		let color = prompt("What color do you want the message to be, if you want a shade of something do it like 'lightblue' not 'light blue'");
+		let j = 1000;
+		for (let i = 0; i < typingMessage.length; i++) {
+			paragraph.style.color = color.toLowerCase();
+			setTimeout(() => paragraph.textContent += typingMessage.value[i], j);
+			j += 50;
+		}
+		typingMessage.style.display = none;
+		typingButton.style.display = none;
+	});
 }

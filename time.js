4754e644b1e3983d65e2a2db45c1ebde2c48a310
BabyOscar11 <muhanli2011@gmail.com>
2020-12-09 13:10:00 -0500

    Update time.js

--- time.js
+++ time.js
@@ -2,14 +2,22 @@ window.onload = function () {
     function getTime () {
         let d = new Date();
         let h1 = document.querySelector('h1');
-        if (d.getMinutes() >= 10 && d.getSeconds() >= 10) {
-            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-        } else if (d.getMinutes() < 10 && d.getSeconds() >= 10) {
-            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:${d.getSeconds()}`;
-        } else if (d.getMinutes() >= 10 && d.getSeconds() < 10) {
-            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:0${d.getSeconds()}`;
+        if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
+            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} AM`;
+        } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
+            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:${d.getSeconds()} AM`;
+        } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() < 12) {
+            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:0${d.getSeconds()} AM`;
+        } else if (d.getMinutes() >= 10 && d.getSeconds >= 10 && d.getHours() < 12) {
+            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:0${d.getSeconds()} AM`;
+        } else if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() > 12 ) {
+            h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:${d.getSeconds()} PM`;
+        } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() > 12) {
+            h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:${d.getSeconds()} PM`;
+        } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() > 12) {
+            h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:0${d.getSeconds()} PM`;
         } else {
-            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:0${d.getSeconds()}`;
+            h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:0${d.getSeconds()} PM`;
         }
     }
     setInterval(() => {

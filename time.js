b8823c0d5e8134c48aaa37314566b8e781f7f422
BabyOscar11 <muhanli2011@gmail.com>
2020-12-09 14:31:40 -0500

    Attempting to add Error handling system

--- time.js
+++ time.js
@@ -1,34 +1,40 @@
 window.onload = function () {
-    function getTime () {
-        let d = new Date();
-        let h1 = document.querySelector("h1");
-        if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
-            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} AM`;
-        } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
-            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:${d.getSeconds()} AM`;
-        } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() < 12) {
-            h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:0${d.getSeconds()} AM`;
-        } else if (d.getMinutes() >= 10 && d.getSeconds >= 10 && d.getHours() < 12) {
-            h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:0${d.getSeconds()} AM`;
-        } else if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() > 12 ) {
-            h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:${d.getSeconds()} PM`;
-        } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() > 12) {
-            h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:${d.getSeconds()} PM`;
-        } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() > 12) {
-            h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:0${d.getSeconds()} PM`;
-        } else {
-            h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:0${d.getSeconds()} PM`;
+    try {
+        function getTime () {
+            let d = new Date();
+            let h1 = document.querySelector("h1");
+            if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
+                h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} AM`;
+            } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
+                h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:${d.getSeconds()} AM`;
+            } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() < 12) {
+                h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:0${d.getSeconds()} AM`;
+            } else if (d.getMinutes() >= 10 && d.getSeconds >= 10 && d.getHours() < 12) {
+                h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:0${d.getSeconds()} AM`;
+            } else if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() > 12 ) {
+                h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:${d.getSeconds()} PM`;
+            } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() > 12) {
+                h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:${d.getSeconds()} PM`;
+            } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() > 12) {
+                h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:0${d.getSeconds()} PM`;
+            } else {
+                h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:0${d.getSeconds()} PM`;
+            }
         }
+        function getDay() {
+            let e = new Date();
+            let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            let h2 = document.querySelector("h2");
+            h2.textContent = `${days[e.getDay()]}, ${months[e.getMonth()]} ${e.getDate()}, ${e.getFullYear()}`;
+        }
+        let myInterval = setInterval(() => {
+            getTime();
+            getDay();
+        }, 100);
+    } catch (err) {
+        document.write("<h1>Oops! There was an error.</h1>");
+        clearInterval(myInterval);
+        console.log(err);
     }
-    function getDay() {
-        let e = new Date();
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let h2 = document.querySelector("h2");
-        h2.textContent = `${days[e.getDay()]}, ${months[e.getMonth()]} ${e.getDate()}, ${e.getFullYear()}`;
-    }
-    setInterval(() => {
-        getTime();
-        getDay();
-    }, 100);
 }

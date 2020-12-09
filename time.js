74b2b081a95638e7a8637532b472a9456502c818
BabyOscar11 <muhanli2011@gmail.com>
2020-12-09 14:17:39 -0500

    Debug

--- time.js
+++ time.js
@@ -24,7 +24,7 @@ window.onload = function () {
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         let h2 = document.querySelector("h2");
-        h2.textContent = `${days[d.getDay()], ${months[d.getMonth()] ${d.getDate()}, ${d.getFullYear()}}`;
+        h2.textContent = `${days[d.getDay()]}, ${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
     }
     setInterval(() => {
         getTime();

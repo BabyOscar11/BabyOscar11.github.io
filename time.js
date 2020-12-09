fa2ae604cf06b29b59dea3a4277090620d345d4b
BabyOscar11 <muhanli2011@gmail.com>
2020-12-09 14:15:29 -0500

    Added day and month and year to time app

--- time.js
+++ time.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     function getTime () {
         let d = new Date();
-        let h1 = document.querySelector('h1');
+        let h1 = document.querySelector("h1");
         if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
             h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} AM`;
         } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
@@ -20,7 +20,14 @@ window.onload = function () {
             h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:0${d.getSeconds()} PM`;
         }
     }
+    function getDay() {
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let h2 = document.querySelector("h2");
+        h2.textContent = `${days[d.getDay()], ${months[d.getMonth()] ${d.getDate()}, ${d.getFullYear()}}`;
+    }
     setInterval(() => {
         getTime();
+        getDay();
     }, 100);
 }

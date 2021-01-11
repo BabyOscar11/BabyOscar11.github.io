e300aa6e6f4be60fdaaa135fa1c4d49782bc98b7
BabyOscar11 <72476617+BabyOscar11@users.noreply.github.com>
2021-01-10 20:35:05 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,25 +1,9 @@
 window.onload = function () {
     try { 
         function getTime () {
-            let d = new Date();
+            let d = Date.now();
             let h1 = document.querySelector("h1");
-            if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
-                h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} AM`;
-            } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() < 12) {
-                h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:${d.getSeconds()} AM`;
-            } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() < 12) {
-                h1.textContent = `The time is ${d.getHours()}:${d.getMinutes()}:0${d.getSeconds()} AM`;
-            } else if (d.getMinutes() >= 10 && d.getSeconds >= 10 && d.getHours() < 12) {
-                h1.textContent = `The time is ${d.getHours()}:0${d.getMinutes()}:0${d.getSeconds()} AM`;
-            } else if (d.getMinutes() >= 10 && d.getSeconds() >= 10 && d.getHours() > 12 ) {
-                h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:${d.getSeconds()} PM`;
-            } else if (d.getMinutes() < 10 && d.getSeconds() >= 10 && d.getHours() > 12) {
-                h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:${d.getSeconds()} PM`;
-            } else if (d.getMinutes() >= 10 && d.getSeconds() < 10 && d.getHours() > 12) {
-                h1.textContent = `The time is ${d.getHours() - 12}:${d.getMinutes()}:0${d.getSeconds()} PM`;
-            } else {
-                h1.textContent = `The time is ${d.getHours() - 12}:0${d.getMinutes()}:0${d.getSeconds()} PM`;
-            }
+            h1.textContent = new Intl.DateTimeFormat("en" , { timeStyle: "medium" }).format(d);
         }
         function getDay() {
             let e = new Date();

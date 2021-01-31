26a23889c5e7cfe4c1899e54a2c544676df16650
BabyOscar11 <muhanli2011@gmail.com>
2021-01-30 19:20:29 -0500

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,38 +3,38 @@ try {
 	while (true) {
 		let rockpaperscissors = function() {
 			let choose = prompt('Rock , Paper , or Scissors?');
-				if (choose.toUpperCase() === 'ROCK') {
-					let choices = ['Rock', 'Paper', 'Scissors'];
-					let AI_choose = choices[Math.floor(Math.random() * 3)];
-					if (AI_choose === 'Rock') {
-						alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Paper') {
-						alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Scissors') {
-						alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`); 
-					}
-				} else if (choose.toUpperCase() === 'PAPER') {
-					let choices = ['Rock', 'Paper', 'Scissors'];
-					let AI_choose = choices[Math.floor(Math.random() * 3)];
-					if (AI_choose === 'Rock') {
-						alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Paper') {
-						alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Scissors') {
-						alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
-					}
-				} else if (choose.toUpperCase() === 'SCISSORS') {
-					let choices = ['Rock', 'Paper', 'Scissors'];
-					let AI_choose = choices[Math.floor(Math.random() * 3)];
-					if (AI_choose === 'Rock') {
-						alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Paper') {
-						alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`);
-					} else if (AI_choose === 'Scissors') {
-						alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
-					}
-				}
-			};
+            if (choose.toUpperCase() === 'ROCK') {
+                let choices = ['Rock', 'Paper', 'Scissors'];
+                let AI_choose = choices[Math.floor(Math.random() * 3)];
+                if (AI_choose === 'Rock') {
+                    alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Paper') {
+                    alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Scissors') {
+                    alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`); 
+                }
+            } else if (choose.toUpperCase() === 'PAPER') {
+                let choices = ['Rock', 'Paper', 'Scissors'];
+                let AI_choose = choices[Math.floor(Math.random() * 3)];
+                if (AI_choose === 'Rock') {
+                    alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Paper') {
+                    alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Scissors') {
+                    alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
+                }
+            } else if (choose.toUpperCase() === 'SCISSORS') {
+                let choices = ['Rock', 'Paper', 'Scissors'];
+                let AI_choose = choices[Math.floor(Math.random() * 3)];
+                if (AI_choose === 'Rock') {
+                    alert(`You lost! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Paper') {
+                    alert(`You win! AI chose ${AI_choose}, and you chose ${choose}!`);
+                } else if (AI_choose === 'Scissors') {
+                    alert(`It's a tie! AI chose ${AI_choose}, and you chose ${choose}!`);
+                }
+            }
+		};
 		if (counter === 0) {
 			rockpaperscissors();
 		}
